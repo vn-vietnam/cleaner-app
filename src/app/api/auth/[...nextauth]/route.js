13cd5546083a8c0020a3fe09dd64d75196affd8a
vnfindjob <vnfindjob@gmail.com>
2024-03-18 22:05:43 +0700

    add

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,43 +1,29 @@
-import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+
 
 export const authOptions = {
-	providers: [
-		{
-			id: "descope",
-			name: "Descope",
-			type: "oauth",
-			wellKnown: `https://api.descope.com/${process.env.DESCOPE_PROJECT_ID}/.well-known/openid-configuration`,
-			authorization: {
-				params: { scope: "openid email profile descope.custom_claims" },
-			},
-			idToken: true,
-			clientId: process.env.DESCOPE_PROJECT_ID,
-			clientSecret: process.env.DESCOPE_ACCESS_KEY,
-			checks: ["pkce", "state"],
-			profile(profile, tokens) {
-				return {
-					id: profile.sub,
-					name: profile.name,
-					email: profile.email,
-					image: profile.picture,
-					idToken: tokens.id_token,
-					...tokens,
-				};
-			},
-		},
-	],
-	secret: process.env.NEXT_PUBLIC_SECRET,
-	callbacks: {
-		async jwt({ token, account }) {
-			if (account?.id_token) {
-				token.idToken = account.id_token;
-			}
-			return token;
-		},
-		async session({ session, token, user }) {
-			// @ts-ignore
-			session.idToken = token.idToken;
-			return session;
-		},
-	},
-};
+    providers: [
+    {
+        id: "descope",
+        name: "Descope",
+        type: "oauth",
+        wellKnown: `https://api.descope.com/`+process.env.DESCOPE_PROJECT_ID+`/.well-known/openid-configuration`,
+        authorization: { params: { scope: "openid email profile" } },
+        idToken: true,
+        clientId: process.env.DESCOPE_PROJECT_ID,
+        clientSecret: process.env.NEXTAUTH_SECRET,
+        checks: ["pkce", "state"],
+        profile(profile) {
+            return {
+                id: profile.sub,
+                name: profile.name,
+                email: profile.email,
+                image: profile.picture,
+            }
+        },
+    }],secret: process.env.NEXTAUTH_SECRET
+}  
+
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
