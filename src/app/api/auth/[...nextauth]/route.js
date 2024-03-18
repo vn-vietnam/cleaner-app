031cb2f7e3ea499a2f06d9b83eef2eb3156f3b74
vnfindjob <vnfindjob@gmail.com>
2024-03-18 21:43:56 +0700

    add

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,29 +1,43 @@
-import NextAuth from "next-auth/next";
-
+import { NextAuthOptions } from "next-auth";
 
 export const authOptions = {
-    providers: [
-    {
-        id: "descope",
-        name: "Descope",
-        type: "oauth",
-        wellKnown: `https://api.descope.com/${process.env.DESCOPE_PROJECT_ID}/.well-known/openid-configuration`,
-        authorization: { params: { scope: "openid email profile" } },
-        idToken: true,
-        clientId: process.env.DESCOPE_ACCESS_KEY,
-        clientSecret: "<Descope Access Key>",
-        checks: ["pkce", "state"],
-        profile(profile) {
-            return {
-                id: profile.sub,
-                name: profile.name,
-                email: profile.email,
-                image: profile.picture,
-            }
-        },
-    }]
-}  
-
-
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+	providers: [
+		{
+			id: "descope",
+			name: "Descope",
+			type: "oauth",
+			wellKnown: `https://api.descope.com/${process.env.DESCOPE_PROJECT_ID}/.well-known/openid-configuration`,
+			authorization: {
+				params: { scope: "openid email profile descope.custom_claims" },
+			},
+			idToken: true,
+			clientId: process.env.DESCOPE_PROJECT_ID,
+			clientSecret: process.env.DESCOPE_ACCESS_KEY,
+			checks: ["pkce", "state"],
+			profile(profile, tokens) {
+				return {
+					id: profile.sub,
+					name: profile.name,
+					email: profile.email,
+					image: profile.picture,
+					idToken: tokens.id_token,
+					...tokens,
+				};
+			},
+		},
+	],
+	secret: process.env.NEXT_PUBLIC_SECRET,
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account?.id_token) {
+				token.idToken = account.id_token;
+			}
+			return token;
+		},
+		async session({ session, token, user }) {
+			// @ts-ignore
+			session.idToken = token.idToken;
+			return session;
+		},
+	},
+};
